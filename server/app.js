d9a5be5da186ec0ccf8f098f50ba3508e32d1ac2
Lucky-1153 <99859107+Lucky-1153@users.noreply.github.com>
2024-08-19 20:42:21 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,6 +29,9 @@ app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
   next();
 });
 
