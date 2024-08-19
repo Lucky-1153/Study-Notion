53143bfd0ee9767b327cf81477a676c8752cba54
Lucky-1153 <99859107+Lucky-1153@users.noreply.github.com>
2024-08-19 21:24:49 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -62,7 +62,9 @@ app.use('/course', courseRoutes)
 
 //============Default Route================================
 app.get('/', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://study-notion-frontend-2ib6.onrender.com');
     res.send('<div> this is default route <p> everything is okay </p> </div>') 
+    console.log('Response headers:', res.headers);
 })
 
 
